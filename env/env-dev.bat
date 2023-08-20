e0d26ada2048a463266cee931ad8629a42e6dbd9
彬腾 <9921523+bin-teng@user.noreply.gitee.com>
2023-08-20 12:06:51 +0800

    feat:全局异常处理

--- env/env-dev.bat
+++ env/env-dev.bat
@@ -5,6 +5,4 @@ set MYSQL_USER=root
 set MYSQL_PASSWORD=root
 set MYSQL_DATABASE=school_shopping
 
-
-@REM user配置
 set USER_SECRET_KEY=SCHOOL_USER_SECRET_KEY
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './common/interceptor';
+import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalInterceptors(new TransformInterceptor());
+
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(3000);
   console.log(`接口运行在 http://localhost:3000`);
